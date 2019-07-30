40175f7e7eeba4d7cb3af478929f16e6b34f41c4
Viet Anh Nguyen <vietanh.phurieng@gmail.com>
2019-07-30 17:02:22 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "assets/css/bootstrap.min.css";
-import "assets/scss/paper-kit.scss";
+import "assets/css/paper-kit.css";
+// import "assets/css/paper-kit.min.css";
+// import "assets/css/paper-kit.css.map";
 import "assets/demo/demo.css";
 import './index.css';
 
